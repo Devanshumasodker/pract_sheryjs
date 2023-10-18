7978404356da94759e9bd96233c3af51fe09417e
Devanshu Masodker <devanshumasodker357@gmail.com>
2023-10-18 20:51:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Example code to initialize Shery.js and use a cool effect.
+
+
+
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr",{videos:["https://cdn.cuberto.com/cb/home/hero/1.mp4"]});
+
+// Shery.mouseFollower({
+//     //Parameters are optional.
+//     skew: true,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
